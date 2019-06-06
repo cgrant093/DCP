cde275f2007f138aac3376de421b14df81f3312a
Unknown <cgrant093@gmail.com>
2019-06-05 20:15:51 -0400

    Couldn't finish 13
    There is something wrong in my logic and I can't figure it out. There's so many situations because the same letters don't have to be next to one another to count.

---
+++ 20190601/DCP20190601.py
@@ -0,0 +1,49 @@
+# find substring function
+def findSubstr(s, k, i):
+    if k == 0:
+        return 1
+
+    if len(s) < k:
+        return 0
+
+    elif i == len(s)-1:
+        return 1
+
+    else:
+
+        if s[i+1] == s[i]:
+            return 1 + findSubstr(s, k, i+1)
+
+        else:
+            letterExists = False
+            for j in range(i):
+                if s[j] == s[i+1]:
+                    letterExists == True
+                    break
+
+            if letterExists:
+                return 1 + findSubstr(s, k, i+1)
+
+            else:
+                notEqWFirst = 1 + findSubstr(s, k-1, i+1)
+                notEqWOFirst = findSubstr(s, k, i+1)
+
+                return max(notEqWFirst, notEqWOFirst)
+
+s = "abcbca"
+k = 2
+print(findSubstr(s, k, 0))
+
+#for i in range(len(s)):
+#    if len(s[i:]) < k:
+ #       print("String doesn't have", k, "unique characters")
+  #      break
+
+   # elif i == len(s)-1 and k == 1
+    #    else:
+     #       ssLength += 1
+
+
+
+
+
